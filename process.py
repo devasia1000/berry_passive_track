23bd846da6a3968da90f2dc5fc9f09694aa2bd04
Devasia Manuel <devasia1000@gmail.com>
2015-01-20 21:44:24 -0600

    anonymized data

--- process.py
+++ process.py
@@ -2,7 +2,11 @@ import json
 
 clam_data = []
 
+time_offset = 45
+lat_offset = 23.3451738
+lon_offset = -42.462398
+
 f = open('clam_data.txt', 'r')
 for line in f:
     t = json.loads(line)
-    print t
+    print t['time'] + time_offset, ',', t['imei'], ',', t['lat'] + lat_offset, ',', t['lon'] + lon_offset
